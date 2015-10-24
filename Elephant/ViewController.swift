b247f8c304301eb684ecdef345200b513cda1b0e
阿狼 <stormte@gmail.com>
2015-10-24 21:55:57 +0800

    Initial Commit

---
+++ Elephant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elephant
+//
+//  Created by admin on 15/10/24.
+//  Copyright (c) 2015年 w3cmm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
